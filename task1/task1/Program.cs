6c57592de285d018aec6e00aa0a22d191d98c05b
Olivinskijt <_email@github.com>
2025-09-29 14:14:00 +0200

    task1 test

---
+++ task1/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Program
+    {
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static string GetMessage(int number)
+        {
+            if (IsEven(number))
+                return "Двері відкриваються!";
+            else
+                return "Двері зачинені...";
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Введіть число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMessage(number));
+        }
+    }
+}
