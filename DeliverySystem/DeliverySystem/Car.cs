96bab1df4ced5284c9897f3486a26902abef19e3
Olivinskijt <st9149201@stud.duikt.edu.ua>
2025-11-19 21:12:45 +0100

    remove

--- DeliverySystem/DeliverySystem/Car.cs
+++ DeliverySystem/DeliverySystem/Car.cs
@@ -5,23 +5,20 @@
         protected int doors;
         protected double fuelLevel;
 
-        // Публічний конструктор Car (для створення Car з default maxSpeed = 180.0)
         public Car(string brand, int year, double mileage, int doors)
             : this(brand, year, mileage, 180.0, doors)
         {
         }
 
-        // Захищений конструктор (для використання класом Van)
         protected Car(string brand, int year, double mileage, double maxSpeed, int doors)
             : base(brand, year, mileage, maxSpeed)
         {
             this.doors = doors;
-            this.fuelLevel = 50; // Початковий рівень палива
+            this.fuelLevel = 50;
         }
 
         public override string GetInfo()
         {
-            // Використовуємо :F2 для форматування числа з двома знаками після коми (для точності, хоча в тестах ціле)
             return $"Car: {brand} ({year}), Doors: {doors}, Fuel: {fuelLevel:F0}L";
         }
 
