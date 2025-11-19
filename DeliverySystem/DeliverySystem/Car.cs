13c0a7530d606dda812e7204b7dcb0278d2577fd
Olivinskijt <st9149201@stud.duikt.edu.ua>
2025-11-19 20:55:31 +0100

    main

---
+++ DeliverySystem/DeliverySystem/Car.cs
@@ -0,0 +1,47 @@
+﻿namespace DeliverySystem
+{
+    public class Car : Vehicle
+    {
+        protected int doors;
+        protected double fuelLevel;
+
+        // Публічний конструктор Car (для створення Car з default maxSpeed = 180.0)
+        public Car(string brand, int year, double mileage, int doors)
+            : this(brand, year, mileage, 180.0, doors)
+        {
+        }
+
+        // Захищений конструктор (для використання класом Van)
+        protected Car(string brand, int year, double mileage, double maxSpeed, int doors)
+            : base(brand, year, mileage, maxSpeed)
+        {
+            this.doors = doors;
+            this.fuelLevel = 50; // Початковий рівень палива
+        }
+
+        public override string GetInfo()
+        {
+            // Використовуємо :F2 для форматування числа з двома знаками після коми (для точності, хоча в тестах ціле)
+            return $"Car: {brand} ({year}), Doors: {doors}, Fuel: {fuelLevel:F0}L";
+        }
+
+        public override void Move(double distance)
+        {
+            base.Move(distance);
+            fuelLevel -= distance * 0.1;
+            if (fuelLevel < 0)
+            {
+                fuelLevel = 0;
+            }
+        }
+
+        public void Refuel(double liters)
+        {
+            fuelLevel += liters;
+            if (fuelLevel > 50)
+            {
+                fuelLevel = 50;
+            }
+        }
+    }
+}
---
+++ DeliverySystem/DeliverySystem/Van.cs
@@ -0,0 +1,40 @@
+﻿namespace DeliverySystem
+{
+    public class Van : Car
+    {
+        private double loadCapacity;
+        private double currentLoad;
+
+        public Van(string brand, int year, double mileage, int doors, double loadCapacity)
+            : base(brand, year, mileage, 140.0, doors) // Виклик захищеного конструктора Car з maxSpeed = 140.0
+        {
+            this.loadCapacity = loadCapacity;
+            this.currentLoad = 0;
+        }
+
+        public override string GetInfo()
+        {
+            // Використовуємо :F0 для форматування числа з двома знаками після коми (для точності, хоча в тестах ціле)
+            return $"Van: {brand} ({year}), Doors: {doors}, Load: {currentLoad:F0}/{loadCapacity:F0}kg, Fuel: {fuelLevel:F0}L";
+        }
+
+        public void LoadCargo(double weight)
+        {
+            if (currentLoad + weight <= loadCapacity)
+            {
+                currentLoad += weight;
+                Console.WriteLine($"{weight} kg loaded into the van.");
+            }
+            else
+            {
+                Console.WriteLine("Too heavy! Cannot load more cargo.");
+            }
+        }
+
+        public void UnloadCargo()
+        {
+            currentLoad = 0;
+            Console.WriteLine("Van unloaded.");
+        }
+    }
+}
